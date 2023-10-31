de77ec73fd541443288adcd2f30ce5e8fbf0968f
Mateusz Minder <mateusz.minder99@gmail.com>
2023-10-31 18:24:05 +0100

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import './Home.css'
+
+export const Home = () => {
+  return (
+    <>
+      <img className="background-image" src="https://i.imgur.com/aRaXPou.png" />
+      {/* Place to put navbar */}
+      <div className="home-page">
+        <header>Welcome to Tibia helper!</header>
+        <p>Tibia helper is a fan-made project to allow users filter and scan some in-game information within just a few clicks on this website.
+          <br />
+          It is designed to be used with ease on every device, so it's accessible literally everywhere without any troubles.
+        </p>
+        <p style={{ color: "red" }}>The text above is about to be replaced when some serious information will require to be displayed here</p>
+      </div>
+    </>
+  )
+}
