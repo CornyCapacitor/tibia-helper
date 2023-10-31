2c1a2da0bfe55c4fbe8a2876be94d24016b43287
Mateusz Minder <mateusz.minder99@gmail.com>
2023-10-31 20:18:54 +0100

    setup for world list

--- src/components/Worlds.tsx
+++ src/components/Worlds.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { Navbar } from './Navbar';
 import './Worlds.css';
 
@@ -15,6 +15,8 @@ type World = {
   game_world_type: string,
   tournament_world_type: string,
 }
+
+type WorldsRender = (value: World[]) => ReactNode;
 
 type GeneralData = {
   players_online: number,
@@ -58,14 +60,47 @@ export const Worlds = () => {
 
   useEffect(fetchWorlds, []);
 
+  const renderWorldList: WorldsRender = (value) => {
+    return value.map(({ name, status, players_online, location, pvp_type, premium_only, transfer_type, battleeye_protected, battleeye_date, game_world_type, tournament_world_type }: World) => (
+      <div key={name} className="world">
+        <div className="world-component">{name}</div>
+        <div className="world-component">{status}</div>
+        <div className="world-component">{players_online}</div>
+        <div className="world-location">{location}</div>
+        <div className="world-pvp-type">{pvp_type}</div>
+        <div className="world-component">{premium_only}</div>
+        <div className="world-component">{transfer_type}</div>
+        <div className="world-component">{battleeye_protected}</div>
+        <div className="world-component">{battleeye_date}</div>
+        <div className="world-component">{game_world_type}</div>
+        <div className="world-component">{tournament_world_type}</div>
+      </div>
+    ));
+  }
+
   return (
     <>
       <img className="background-image" src="https://c.wallhere.com/images/dc/db/bf0f2b38fab4fe5804ce04563ae2-1706939.jpg!d" />
       <Navbar />
       <div className="worlds-page">
-        <button onClick={() => console.log(worldsData)}>Log the worlds</button>
-        <button onClick={() => console.log(generalData)}>Log the general data</button>
+        <div className="worlds-table">
+          <div className="world">
+            <div className="world-component">Name:</div>
+            <div className="world-component">Status:</div>
+            <div className="world-component">Players online:</div>
+            <div className="world-location">Location:</div>
+            <div className="world-pvp-type">Pvp type:</div>
+            <div className="world-component">Premium only:</div>
+            <div className="world-component">Transfer type:</div>
+            <div className="world-component">Battleeye protected:</div>
+            <div className="world-component">Battleeye date:</div>
+            <div className="world-component">Game world type:</div>
+            <div className="world-component">Tournament world type:</div>
+          </div>
+          {renderWorldList(worldsData ?? [])}
+        </div>
       </div>
+
     </>
   )
 }
