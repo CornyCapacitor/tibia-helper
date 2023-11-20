a61a8e0a3e34712e72c3f572c616ef315364c01b
Mateusz Minder <mateusz.minder99@gmail.com>
2023-11-20 14:08:45 +0100

    changed component/page relation logic

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,11 +1,16 @@
 import { Characters } from "../components/Characters";
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
 import { ScrollToTop } from "../components/SctollToTop";
 
 export const CharactersPage = () => {
   ScrollToTop();
   return (
     <>
+      <img className="background-image" src="https://wallpapercave.com/wp/wp7219191.jpg" />
+      <Navbar />
       <Characters />
+      <Footer />
     </>
   )
 }
